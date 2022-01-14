16ac95c18cf83a5dcb1ab8d9a81c8b46625933bb
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-14 21:56:42 +0530

    police verification details

--- controllers/userDetail.controller.js
+++ controllers/userDetail.controller.js
@@ -43,56 +43,37 @@ const userDetailController = {
     console.log(req.body);
     if (role === 5000) {
       const policeInfoSchema = Joi.object({
-        adharCard: Joi.string().pattern(
+        adhaarCard: Joi.string().pattern(
           new RegExp("^[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$")
         ),
         panCard: Joi.string().pattern(new RegExp("[A-Z]{5}[0-9]{4}[A-Z]{1}")),
-        policeId: Joi.string().min(3).max(30),
-        postingArea: Joi.string(),
+        policeID: Joi.string().min(3).max(30),
         stationName: Joi.string().min(3).max(30),
+        postingArea: Joi.string(),
         policePost: Joi.string().min(3).max(30),
         city: Joi.string(),
         dob: Joi.date(),
-        address: Joi.string().min(3).max(300),
+        address: Joi.string().min(3).max(500),
+        postingAreaAddress: Joi.string().min(10).max(500),
       });
       const { error } = policeInfoSchema.validate(req.body);
       if (error) {
         return next(error);
       }
-      const {
-        adharCard,
-        panCard,
-        policeId,
-        postingArea,
-        stationName,
-        policePost,
-        dob,
-        address,
-      } = req.body;
-
-      const info = await User.findById(_id);
-
-      const data = {
-        adharCard: adharCard ? adharCard : info.userDetails.adharCard,
-        panCard: panCard ? panCard : info.userDetails.panCard,
-        policeId: policeId ? policeId : info.userDetails.policeId,
-        postingArea: postingArea ? postingArea : info.userDetails.postingArea,
-        stationName: stationName ? stationName : info.userDetails.stationName,
-        policePost: policePost ? policePost : info.userDetails.policePost,
-        dob: dob ? dob : info.userDetails.dob,
-        address: address ? address : info.userDetails.address,
-      };
       //
       const updateDetails = await User.findOneAndUpdate(
         { _id: _id },
         {
           $set: {
-            userDetails: data,
+            userDetails: req.body,
           },
         }
       );
 
-      return res.json(updateDetails);
+      return res.json({
+        success: true,
+        updateDetails,
+      });
     }
   },
   async uploadProfile(req, res, next) {
