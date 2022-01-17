f8587b0ddb993c590133f078e75b5379f9169bdd
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-17 22:54:06 +0530

    cors fix

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(cookieParser());
-// const corsOptions = {
-//   credentials: true,
-//   origin: [
-//     "http://localhost:3000",
-//     "http://localhost:19002/",
-//     "http://192.168.0.108:19002/",
-//   ],
-// };
+const corsOptions = {
+  credentials: true,
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:19002/",
+    "http://192.168.0.108:19002/",
+  ],
+};
 
 mongoose.connect(DBURL, {
   useNewUrlParser: true,
@@ -29,30 +29,8 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("Db Connected"));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Pass to next layer of middleware
-  next();
-});
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", router);
