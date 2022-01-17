e5ba2769d42423d26ac008d90b4daefaec34fdad
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-17 22:49:12 +0530

    cors fix

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ const app = express();
 app.use(cookieParser());
 // const corsOptions = {
 //   credentials: true,
-//   origin: "*",
-//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
+//   origin: [
+//     "http://localhost:3000",
+//     "http://localhost:19002/",
+//     "http://192.168.0.108:19002/",
+//   ],
 // };
 
 mongoose.connect(DBURL, {
@@ -28,7 +29,29 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("Db Connected"));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
