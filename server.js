3ab8a0a31212fcdbd31d78199c677571a1361726
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-18 01:47:42 +0530

    cors fix

--- server.js
+++ server.js
@@ -31,7 +31,10 @@ db.on("error", console.error.bind(console, "connection error"));
 db.once("open", () => console.log("Db Connected"));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 app.use(cors(corsOptions));
 app.use(express.json());
 
