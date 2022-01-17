d87ef71dbae859fcddbe92459afb9367fc90f37d
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2022-01-17 22:35:56 +0530

    cors fix

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(cookieParser());
-const corsOptions = {
-  credentials: true,
-  origin: [],
-};
+// const corsOptions = {
+//   credentials: true,
+//   origin: "*",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204,
+// };
 
 mongoose.connect(DBURL, {
   useNewUrlParser: true,
@@ -26,7 +29,7 @@ db.once("open", () => console.log("Db Connected"));
 // app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
